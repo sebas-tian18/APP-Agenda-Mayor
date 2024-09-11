f7a3117275c947665e7a3108e42e15a513ca98e1
Bad4steroid <sesparza2020@alu.uct.cl>
2024-09-11 16:37:38 -0300

    Anadir variables de entorno para conexion a BD

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
