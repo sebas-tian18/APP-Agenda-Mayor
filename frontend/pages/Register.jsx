3d7e9755cf6baa4170b20c409b278060e91f78cb
der-kaffe <cafeconcafeina@gmail.com>
2024-09-11 20:14:31 -0300

    Rehacer el Register para que quede más limpio y con letras más grandes

--- frontend/pages/Register.jsx
+++ frontend/pages/Register.jsx
@@ -1,175 +1,180 @@
 const Register = () => {
   return (
     <div
-      className="min-h-screen min-w-screen bg-cover bg-center"
+      className="min-h-screen w-full bg-cover bg-center text-wrap text-3xl"
       style={{
         backgroundImage: "url('https://picsum.photos/seed/picsum/200/300')",
       }}
     >
-      <div className="min-h-screen bg-black bg-opacity-50 flex items-center ">
-        <div className="basis-1/3"></div>
-        <div className="basis-1/3 bg-white shadow-lg  flex min-w-[900px] ">
-          <div className="bg-gradient-to-b from-emerald-500 to-indigo-500 basis-1/3 mr-3"></div>
-          <div className=" basis-2/3 ">
-            <h2 className="text-2xl font-bold p-6 ">
-              Registrarse en la plataforma
-            </h2>
-            <form className="p-2">
-              <div className="mb-4  ">
-                <label className="block text-gray-700">Nombres:</label>
-                <input
-                  type="text"
-                  name="nombres"
-                  className="min-w-full my-1 border-b-2 border-[#FF5100] outline-0"
-                  placeholder="Nombre1 Nombre2 Nombre3"
-                ></input>
-              </div>
-              <div className="mb-4 grid grid-cols-2 flex-col ">
-                <div>
-                  <label className="block text-gray-700">
-                    Apellido Paterno:
-                  </label>
+      <div className="min-h-screen bg-black bg-opacity-50 flex items-center">
+        {/* Aquí ajustamos la estructura según el tamaño de pantalla */}
+        <div className="w-full max-w-7xl mx-auto px-4">
+          <div className="flex flex-col md:flex-row bg-white shadow-lg min-h-[600px]">
+            <div className="bg-gradient-to-b from-emerald-500 to-indigo-500 basis-full md:basis-1/3"></div>
+
+            <div className="basis-full md:basis-2/3 p-6">
+              <h2 className="text-2xl font-bold mb-6 text-center md:text-left">
+                Registrarse en la plataforma
+              </h2>
+              <form className="p-2">
+                {/* Campo Nombres */}
+                <div className="mb-4">
+                  <label className="block text-gray-700">Nombres:</label>
                   <input
                     type="text"
-                    name="ApellidoP"
-                    className=" my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="AAAAAAAAA"
-                  ></input>
-                </div>
-                <div>
-                  <label>Apellido Materno:</label>
-                  <input
-                    type="text"
-                    name="ApellidoP"
-                    className="w-full my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="BBBBBBBBB"
-                  ></input>
-                </div>
-              </div>
-              <div className="mb-4 grid grid-cols-2 flex-col">
-                <div>
-                  <label className="block text-gray-700">RUT: </label>
-                  <input
-                    type="text"
-                    name="rut"
-                    className="min-w-FIT my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="123456789-0"
-                  ></input>
-                </div>
-                <div className="mb-6">
-                  <label className="block text-gray-700">
-                    Registro Social de Hogares:
-                  </label>
-                  <input
-                    type="file"
-                    name="archivo"
-                    className="block w-full text-sm border-gray-300 cursor-pointer outline-none"
-                    id="file_input"
+                    name="nombres"
+                    className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                    placeholder="Nombre1 Nombre2 Nombre3"
                   />
                 </div>
-              </div>
 
-              <div className="mb-4 grid grid-cols-2  flex-col">
-                <div className="mb-4">
-                  <label className="block text-gray-700">Correo: </label>
-                  <input
-                    type="email"
-                    name="correo"
-                    className="min-w-FIT my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="correo@jemplo.com"
-                  ></input>
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-700">Contraseña: </label>
-                  <input
-                    type="password"
-                    name="contrasena"
-                    className="min-w-FIT my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="***************"
-                  ></input>
-                </div>
-              </div>
-
-              <div className="mb-4 grid grid-cols-2  flex-col">
-                <div className="mb-4">
-                  <label className="block text-gray-700">
-                    Repetir contraseña:{" "}
-                  </label>
-                  <input
-                    type="password"
-                    name="Rcontrasena"
-                    className="min-w-fit my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="***************"
-                  ></input>
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-700">Telefono: </label>
-                  <input
-                    type="text"
-                    name="telefono"
-                    className="min-w-fit my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="+569 XXXXXXXX"
-                  ></input>
-                </div>
-              </div>
-              <div className="mb-4 grid grid-cols-2  flex-col">
-                <div className="mb-4">
-                  <label className="block text-gray-700">Direccion: </label>
-                  <input
-                    type="text"
-                    name="direccion"
-                    className="min-w-FIT my-1 border-b-2 border-[#FF5100] outline-0"
-                    placeholder="Calle falsa 123"
-                  ></input>
-                </div>
-                <div className="mb-4">
-                  <label className="block text-gray-700 ">Sexo: </label>
-                  <select
-                    name="sexo"
-                    className=" min-w-fit my-1 border-b-2 border-[#FF5100] outline-0"
-                  >
-                    <option value="M">Masculino</option>
-                    <option value="F">Femenino</option>
-                  </select>
-                </div>
-              </div>
-              <div className="mb-4 grid grid-cols-2  flex-col">
-                <div className="mb-4">
-                  <label className="block text-gray-700 ">Nacionalidad: </label>
-                  <select
-                    name="nacionalidad"
-                    className=" min-w-fit my-1 border-b-2 border-[#FF5100] outline-0"
-                  >
-                    <option value="CL">Chilena</option>
-                    <option value="Ex">Extranjero</option>
-                  </select>
+                {/* Campos Apellidos */}
+                <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label className="block text-gray-700">Apellido Paterno:</label>
+                    <input
+                      type="text"
+                      name="ApellidoP"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="Apellido Paterno"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Apellido Materno:</label>
+                    <input
+                      type="text"
+                      name="ApellidoM"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="Apellido Materno"
+                    />
+                  </div>
                 </div>
 
-                <div className="mb-4">
-                  <label className="block text-gray-700 ">
-                    Problemas de movilidad:{" "}
-                  </label>
-                  <select
-                    name="sexo"
-                    className=" min-w-fit my-1 border-b-2 border-[#FF5100] outline-0"
-                  >
-                    <option value="Trsh">Si</option>
-                    <option value="Frsh">Extranjero</option>
-                  </select>
+                {/* Campo RUT y Archivo */}
+                <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label className="block text-gray-700">RUT:</label>
+                    <input
+                      type="text"
+                      name="rut"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="123456789-0"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Registro Social de Hogares:</label>
+                    <input
+                      type="file"
+                      name="archivo"
+                      className="block w-full text-sm border-gray-300 cursor-pointer outline-none"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="w-full flex justify-center items-center">
-              <button
-                type="submit"
-                className="min-w-48 bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-700 "
-              >
-                Registrarse
-              </button>
-              </div>
-            </form>
+
+                {/* Campo Correo y Contraseña */}
+                <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label className="block text-gray-700">Correo:</label>
+                    <input
+                      type="email"
+                      name="correo"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="correo@ejemplo.com"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Contraseña:</label>
+                    <input
+                      type="password"
+                      name="contrasena"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="***************"
+                    />
+                  </div>
+                </div>
+
+                {/* Repetir Contraseña y Teléfono */}
+                <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label className="block text-gray-700">Repetir Contraseña:</label>
+                    <input
+                      type="password"
+                      name="Rcontrasena"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="***************"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Teléfono:</label>
+                    <input
+                      type="text"
+                      name="telefono"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="+569 XXXXXXXX"
+                    />
+                  </div>
+                </div>
+
+                {/* Dirección y Sexo */}
+                <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label className="block text-gray-700">Dirección:</label>
+                    <input
+                      type="text"
+                      name="direccion"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                      placeholder="Calle falsa 123"
+                    />
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Sexo:</label>
+                    <select
+                      name="sexo"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                    >
+                      <option value="M">Masculino</option>
+                      <option value="F">Femenino</option>
+                    </select>
+                  </div>
+                </div>
+
+                {/* Nacionalidad y Problemas de Movilidad */}
+                <div className="mb-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <div>
+                    <label className="block text-gray-700">Nacionalidad:</label>
+                    <select
+                      name="nacionalidad"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                    >
+                      <option value="CL">Chilena</option>
+                      <option value="Ex">Extranjero</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Problemas de Movilidad:</label>
+                    <select
+                      name="movilidad"
+                      className="w-full my-1 border-b-2 border-[#FF5100] outline-none"
+                    >
+                      <option value="Si">Sí</option>
+                      <option value="No">No</option>
+                    </select>
+                  </div>
+                </div>
+
+                {/* Botón Registrarse */}
+                <div className="flex justify-center">
+                  <button
+                    type="submit"
+                    className="w-full md:w-auto bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-700"
+                  >
+                    Registrarse
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-        <div className="basis-1/3"></div>
       </div>
     </div>
   );
