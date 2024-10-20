5bb88fea84a45d9c77a3aec39d08a62dfcffd813
Bad4steroid <sesparza2020@alu.uct.cl>
2024-10-20 17:04:04 -0300

    Actualizar middleware de verificacion del JWT

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,17 +7,24 @@ function verifyToken(req, res, next) {
     const authHeader = req.headers.authorization; // Obtener el token del header ('Bearer <token>')
   
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: "Token no recibido o formato inválido" });
+        return res.status(401).json({ message: "Token no recibido o formato no valido" });
     }
     
     const token = authHeader.split(' ')[1]; // Extraer el token despues de 'Bearer '
   
     try {
         const payload = verificarToken(token); // Verificar el token
-        req.username = payload.username; // Guardar payload en req
-        next();
+        req.username = payload; // Guardar payload en req
+        next(); // Continuar al siguiente middleware o controlador
     } catch (error) {
-        return res.status(403).json({ message: "Token no válido" });
+        switch (error.name) {
+            case 'TokenExpiredError':
+                return res.status(401).json({ message: "Token expirado" });
+            case 'JsonWebTokenError':
+                return res.status(403).json({ message: "Token no válido" });
+            default:
+                return res.status(500).json({ message: "Error al verificar el token" });
+        }
     }
   }
 
